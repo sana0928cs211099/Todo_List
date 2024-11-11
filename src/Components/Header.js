7f28688f0fd0065d49df379eb07801ea76aedb14
Sana Siddiqui <132052506+sana0928cs211099@users.noreply.github.com>
2024-11-11 15:24:00 +0530

    First Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Make your list</h1>
+        </div>
+    );
+};
+
+export default Header;
